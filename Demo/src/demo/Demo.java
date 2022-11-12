5a30a7460c3d2b0445aac6ea3a19547b566f8e92
tiricnguyen <tiencong355@gmail.com>
2022-11-12 13:45:42 +0700

    commit

---
+++ Demo/src/demo/Demo.java
@@ -0,0 +1,23 @@
+/*                          
+                            +====================+
+                            |                    |
+                            |     TIRICNGUYEN    |
+                            |                    |
+                            +====================+  
+ */
+package demo;
+
+/**
+ *
+ * @author Admin
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("helllo");
+    }
+    
+}
